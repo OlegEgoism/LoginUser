e695c2a1edf9e678320d2e96578ff834cdc31de5
OlegEgoism <olegpustovalov220@gmail.com>
2023-08-23 16:05:33 +0300

    docker

--- udp_login/admin.py
+++ udp_login/admin.py
@@ -24,8 +24,7 @@ class UserInfoAdmin(ImportExportModelAdmin):
     resource_class = UserInfoResource
     list_display = 'user_name', 'login_user', 'password_user', 'note', 'is_active', 'date_created', 'date_updated',
     list_editable = 'note', 'is_active',
-    list_filter = ("date_created", DateRangeFilterBuilder()), (
-        "date_updated", DateRangeFilterBuilder()), 'is_active', 'note',
+    list_filter = ("date_created", DateRangeFilterBuilder()), ("date_updated", DateRangeFilterBuilder()), 'is_active', 'note',
     search_fields = 'user_name', 'note', 'login_user',
     search_help_text = 'Поиск по пользователю, логину и примечанию'
     list_per_page = 20
